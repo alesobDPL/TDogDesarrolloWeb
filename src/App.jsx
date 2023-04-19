7f9cc8297a6090d0aeba6fc207e5e78f1c29d23c
alesobDPL <alejandrosobarzo56@gmail.com>
2023-04-19 14:58:28 -0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,78 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import {
   Box,
   Card,
   CardContent,
   CardMedia,
   Grid,
-  TextField,
+  CircularProgress,
 } from "@mui/material";
 import axios from "axios";
 
 function App() {
+  const [Loading,setLoading] = useState(false);
+  const [DesactivateButton,setDesactivateButton] = useState(false);
   const [ListadoDerecha, setListadoDerecha] = useState([]);
   const [ListadoIzquierda, setListadoIzquierda] = useState([]);
-  const [dogLover,setdogLover] = useState({
-    label:null,
-    image:null
-  })
+  const [dogLover, setDogLover] = useState({
+    label: null,
+    image: null,
+  });
 
-
-  const getDog = () => {
-    axios
-      .get("https://dog.ceo/api/breeds/image/random")
-      .then((response) => {
-
-        //Create a random name
-        const name = nombreRandom();
-
-        //Override the dogLover properties.
-        setdogLover({
-          ...dogLover,
-          image: response.data.message,
-          label: name
-        });
-
-        //Show what's in it.
-        console.log(dogLover)
-        
-
-
-      });
+  const getDog = async () => {
+    setDesactivateButton(true)
+    setLoading(true); // Set loading state to true
+    const response = await axios.get("https://dog.ceo/api/breeds/image/random");
+    // Create a random name
+    const name = nombreRandom();
+  
+    //Override the dogLover properties.
+    setDogLover({
+      ...dogLover,
+      image: response.data.message,
+      label: name,
+    });
+  
+    // Set loading state to false
+    setDesactivateButton(false)
+    setLoading(false);
+    
   };
 
-  function nombreRandom(){
-    let res = '';
-    for(let i = 0; i < 6; i++){
-        const random = Math.floor(Math.random() * 25);
-        res += String.fromCharCode(97 + random);
-    };
+  function nombreRandom() {
+    let res = "";
+    for (let i = 0; i < 6; i++) {
+      const random = Math.floor(Math.random() * 25);
+      res += String.fromCharCode(97 + random);
+    }
     return res;
-}
+  }
 
   useEffect(() => {
     getDog();
-    },[])
-  console.log("oa aca dog lover",dogLover);
-
-
-
-
-
-
-  {/*
-  useEffect(() => {
-    if (buscador.trim() !== "") {
-      let result = listado.filter((item) =>
-        item.name.toString().includes(buscador.toString().trim())
-      );
-      setListaAux(listaAux.concat(result));
-    }else {
-      setListaAux([]);
-    }
-  }, [buscador]);
-*/}
-
-  //      https://javascript.plainenglish.io/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
-
-
+  }, []);
 
   const aceptar = (item) => {
-    setListadoDerecha(ListadoDerecha.concat(item))
+    setListadoDerecha(ListadoDerecha.concat(item));
     getDog();
   };
 
   const rechazar = (item) => {
-    setListadoIzquierda(ListadoIzquierda.concat(item))
+    setListadoIzquierda(ListadoIzquierda.concat(item));
     getDog();
   };
 
-
   const aceptarContrario = (objeto) => {
-    setListadoDerecha(ListadoDerecha=>[...ListadoDerecha,objeto])
-    setListadoIzquierda(ListadoIzquierda.filter(x=> x != objeto))
-  }
+    setListadoDerecha((ListadoDerecha) => [...ListadoDerecha, objeto]);
+    setListadoIzquierda(ListadoIzquierda.filter((x) => x != objeto));
+  };
 
   const rechazarContrario = (objeto) => {
-    setListadoIzquierda(ListadoIzquierda=>[...ListadoIzquierda,objeto])
-    setListadoDerecha(ListadoDerecha.filter(x=> x != objeto))
-  }
-
-
-
-
-
+    setListadoIzquierda((ListadoIzquierda) => [...ListadoIzquierda, objeto]);
+    setListadoDerecha(ListadoDerecha.filter((x) => x != objeto));
+  };
 
 
 
@@ -142,23 +112,28 @@ function App() {
         </Grid>
 
         <Grid item md={4} xs={4} sx={{ background: "green" }}>
-        <Card>
-            
-            <CardMedia
-              component="img"
-              image={dogLover?.image}
-              sx={{ width: 250 }}
-              // ver como ajustar url
-            />
-
-            <CardContent>
-              nombre: {dogLover?.label}<br />
-              <button onClick={() => rechazar(dogLover)}>Rechazar</button>
-              <button onClick={() => aceptar(dogLover)}>Aceptar</button>
-            </CardContent>
-            
-          </Card>
-        </Grid>
+  <Card>
+    {Loading ? ( // Conditionally render loading spinner
+      <Box display="flex" justifyContent="center" alignItems="center" height="250px">
+        <CircularProgress size={80} />
+      </Box>
+    ) : (
+      <Box>
+        <CardMedia
+          component="img"
+          image={dogLover?.image}
+          sx={{ width: 250 }}
+        />
+      </Box>
+      
+    )}
+  </Card>
+  <CardContent>
+          nombre: {dogLover?.label}<br />
+          <button  disabled={DesactivateButton} onClick={() => rechazar(dogLover)}>Rechazar</button>
+          <button  disabled={DesactivateButton} onClick={() => aceptar(dogLover)}>Aceptar</button>
+        </CardContent>
+</Grid>
 
         <Grid item md={4} xs={4} sx={{ background: "blue" }}>
         {ListadoDerecha.map((item,index) => (
